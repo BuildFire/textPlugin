e2f001f4c4abb83e7a9f7d12bd1652c634efdc47
alialaqrabawi3 <aalaqrabawi@madaincorp.com>
2024-07-03 21:14:11 +0300

    move state file to control

--- src/control/content/controlState.js
+++ src/control/content/controlState.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-unused-vars
-const AppState = {
+const ControlState = {
     settings: null,
 };
--- src/widget/global/js/repositories/Settings.js
+++ src/widget/global/js/repositories/Settings.js
@@ -21,12 +21,12 @@ class Settings {
                     const data = new Setting().toJSON();
                     data.createdBy = currentUser?._id ? currentUser._id : "";
                     Settings.save(data).then(()=>{
-                        AppState.settings = data;
+                        ControlState.settings = data;
                         resolve(data);
                     })
                 } else {
                     const data = new Setting(res.data).toJSON();
-                    AppState.settings = data;
+                    ControlState.settings = data;
                     resolve(data);
                 }
             });
@@ -42,8 +42,8 @@ class Settings {
         return new Promise((resolve, reject) => {
             const currentUser = AuthManager.currentUser;
             data.lastUpdatedBy = currentUser?._id ? currentUser._id : "";
-            data.createdBy = data.createdBy || AppState.settings?.createdBy;
-            data.createdOn = AppState.settings?.createdOn || new Date();
+            data.createdBy = data.createdBy || ControlState.settings?.createdBy;
+            data.createdOn = ControlState.settings?.createdOn || new Date();
             buildfire.datastore.save(new Setting(data).toJSON(), Settings.TAG, (err, res) => {
                 if (err) return reject(err);
                 return resolve(new Setting(res.data).toJSON());
